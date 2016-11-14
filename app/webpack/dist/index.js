7031f585a66ff0efeb1aafeeabfdbf16784abec6
1243817329@qq.com <520huihui>
2016-11-14 15:31:50 +0800

    --webpack

---
+++ app/webpack/dist/index.js
@@ -0,0 +1,8 @@
+import '../scss/index.scss';
+console.log("ok");
+let  a ="我是辉辉~";
+console.log(a);
+let b =["1","2","3","4","5"];
+for (var value of b) {
+    console.log(value);
+}
--- app/webpack/webpack.config.js
+++ app/webpack/webpack.config.js
@@ -2,37 +2,56 @@
  * Created by Administrator on 2016/10/26.
  */
 var webpack = require('webpack');
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
-
 module.exports = {
-    //插件项
-    plugins: [commonsPlugin],
+    /*    //插件项
+     plugins: [commonsPlugin],*/
     //页面入口文件配置
     entry: {
-        index : './src/js/page/index.js'
+        index: './dist/index'
     },
     //入口文件输出配置
     output: {
-        path: 'dist/js/page',
+        path: './js/',
         filename: '[name].js'
     },
     module: {
         //加载器配置
         loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.js$/, loader: 'jsx-loader?harmony' },
-            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+             /*
+            //使用ES6
+            //# npm install babel-loader --save-dev
+            //# npm install babel-core --save
+            //# npm install babel-preset-es2015 --save-dev
+            //使用ES7
+            //# npm install --save-dev babel-preset-stage-0
+            //# npm install --save-dev babel-preset-stage-1
+            //# npm install --save-dev babel-preset-stage-2
+            //# npm install --save-dev babel-preset-stage-3
+            */
+            {
+                test: /\.js$/,
+                loader: 'babel?presets[]=es2015'
+            },
+            //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
+            //npm install node-sass --save-dev
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+          /*  {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.js$/, loader: 'jsx-loader?harmony'},
+            {test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}*/
         ]
     },
-    //其它解决方案配置
-    resolve: {
-        root: 'E:/github/flux-example/src', //绝对路径
-        extensions: ['', '.js', '.json', '.scss'],
-        alias: {
-            AppStore : 'js/stores/AppStores.js',
-            ActionType : 'js/actions/ActionType.js',
-            AppAction : 'js/actions/AppAction.js'
-        }
-    }
+    // //其它解决方案配置
+    // resolve: {
+    //     root: 'E:/github/flux-example/src', //绝对路径
+    //     extensions: ['', '.js', '.json', '.scss'],
+    //     alias: {
+    //         AppStore : 'js/stores/AppStores.js',
+    //         ActionType : 'js/actions/ActionType.js',
+    //         AppAction : 'js/actions/AppAction.js'
+    //     }
+    // }
 };
