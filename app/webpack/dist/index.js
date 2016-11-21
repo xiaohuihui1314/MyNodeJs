9f93cd8e7a1545eb3c01bbeb7f243120f2e75f8e
1243817329@qq.com <520huihui>
2016-11-21 17:41:20 +0800

    --JavaScript

--- app/webpack/dist/index.js
+++ app/webpack/dist/index.js
@@ -3,11 +3,16 @@ let a = ["1", "2", "3", "4", "5"];
 for (var value of a) {
     console.log(value);
 }
-var input = [1, 2, 1, 3];
-input.map(item => item + 1);
-console.log(input);
+var arry = [1, 2, 1, 3];
+arry.forEach(function (value) {
 
-var un = undefined;
+});
 
-console.log(  un == undefined);
 
+{
+    let rand =Math.random(),num =(rand*0x1000000<<0).toString(),sum=0,lucky;
+    for(var i of num){
+        sum =sum+~~i;
+    }
+    lucky= (sum%9)?1:0;
+}
